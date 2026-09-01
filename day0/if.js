7753907f9421c1c0999fec3d2f4c3bbe09cb7504
issam <bousataissam575@gmail.com>
2026-09-01 13:54:56 +0100

    day0 if condition exercice

---
+++ day0/if.js
@@ -0,0 +1,28 @@
+// exercice 1
+let age = 15 ;
+age = 32
+if (age < 18) console.log("you cannot enter");
+else console.log("you can enter");
+
+// exercice 2
+ let note = 12 ;
+
+ if (note < 20 &&  18 < note) console.log("excelent");
+ else if ( 16 < note && note <= 17) console.log("very good");
+ else if (14 < note && note <= 15) console.log("good");
+ else if (10 <= note && note <= 13) console.log("pass");
+ else if (note < 10) console.log("fail");
+ 
+ 
+// exercice 3
+
+let number = -27.2  ;
+
+if (number < 0) console.log("negatif");
+else if (number > 0) console.log("positif");
+else  console.log("egal zero");
+ 
+
+// exercice 4
+
+
