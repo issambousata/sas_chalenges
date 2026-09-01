da318dd12e1c0dae8d6a0b174018cd77acd0c07e
issam <bousataissam575@gmail.com>
2026-09-01 11:23:48 +0100

    day0 let and const exercices

---
+++ day0/const.js
@@ -0,0 +1,28 @@
+// exercice 1
+const country = "morocco" ;
+console.log(country);
+
+
+// exercice 2
+const birthYear = 2003 ;
+const nowYear = 2026 ;
+const myAge =nowYear - birthYear  ;
+console.log(myAge);
+
+// exercice 3
+const name = "issam" ;
+// name = "Ahmed" ; // Error
+console.log(name);   
+
+// exercice 4
+const produit_name = "computer" ;
+let price = 5000 ;
+let quantity = 4 ;
+
+price = 4800 ;
+quantity = 56 ;
+
+console.log("nom de produit : " + produit_name);
+console.log("prix de produit : " + price);
+console.log("quantity : " + quantity);
+
---
+++ day0/let.js
@@ -0,0 +1,32 @@
+// exercice 1
+
+let age = 23 ;
+console.log(age);
+
+// exercice 2
+
+let name = "issam";
+let age2 = 22 ;
+let city = "demnate" ;
+
+console.log(name);
+console.log("my name is " + name + "and I am " + age2 + "and I am from in " + city );
+
+
+// exercice 3
+
+let prix = 34 ;
+prix = 40 ;
+ console.log(prix);
+
+
+// exercice 4
+
+let note1 = 15 ;
+let note2 = 17 ;
+let note3 = 12 ;
+let somme = note1 + note2 + note3  ;
+console.log(some);
+
+
+
